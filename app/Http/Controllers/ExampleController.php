f243a5499a9f9d99ecbcf94672f0aa1925744717
Ezequiel Pereira <ezequielsp@users.noreply.github.com>
2017-09-11 20:21:12 -0300

    All files

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function test(Request $request)
+    {
+
+        dd($request->auth->toArray());
+
+        return response()->json(['teste']);
+    }
+
+    //
+}
---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Exception;
+use App\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\ExpiredException;
+
+class JwtMiddleware
+{
+    public function handle($request, Closure $next, $guard = null)
+    {
+        $header = $request->header('authorization');
+        $token  = null;
+        if (!empty($header)) {
+            if (preg_match('/Bearer\s(\S+)/', $header, $matches)) {
+                $token = $matches[1];
+            }
+        } else {
+            $token = $request->get('token');
+        }
+
+        if(!$token) {
+            // Unauthorized response if token not there
+            return response()->json([
+                'error' => 'Token not provided.'
+            ], 401);
+        }
+
+        try {
+            $credentials = JWT::decode($token, env('JWT_SECRET'), ['HS256']);
+        } catch(ExpiredException $e) {
+            return response()->json([
+                'error' => 'Provided token is expired.'
+            ], 400);
+        } catch(Exception $e) {
+            return response()->json([
+                'error' => 'An error while decoding token.'
+            ], 400);
+        }
+        
+        $user = User::find($credentials->sub);
+
+        // Now let's put the user in the request class so that you can grab it from there
+        $request->auth = $user;
+
+        return $next($request);
+    }
+}
+
